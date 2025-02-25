fea45572f1277df64cf985f19d40d9ffd3652bbf
naveedbasharat06 <naveed.basharat06@gmail.com>
2025-02-26 04:38:41 +0500

    nav, heroSection responsiness design

--- src/components/herosection/HeroSection.tsx
+++ src/components/herosection/HeroSection.tsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
       .join(" ");
   };
   return (
-    <div className="flex bg-white w-full mx-auto">
+    <div className="hero_section_main bg-white w-full mx-auto">
       <div className="hero_section_content">
         <img className="relative left-14 mt-4" src={start03} alt="" />
         <img
@@ -28,33 +28,37 @@ const HeroSection = () => {
           alt="bg grandiant"
         />
         <div className="heroSection_text_bg">
-          <h2 className="text-6xl font-bold w-[78%]">
+          <h2 className="">
             {toTitleCase("make the best financial decisions")}
           </h2>
-          <p className="text-gray-500 w-[616px] mt-6">
+          <p className="text-gray-500 w-[616px] mt-6 z-10">
             {toTitleCase(`Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
           faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.`)}
           </p>
         </div>
         {/* Get starter and watch video */}
-        <div className="heroSection_GetStarted flex relative bottom-96 left-28 gap-7">
-          <button className="get_started items-center bg-black text-white px-7 py-4 hidden md:flex rounded-sm font-medium">
+        <div className="heroSection_GetStarted">
+          <button className="get_started items-center bg-black text-white px-7 py-4  md:flex rounded-sm font-medium">
             Get Started ➡
           </button>
           <span className="flex gap-2">
-            <img src={heroWatchIcon} alt="" />
+            <img className="watch_video" src={heroWatchIcon} alt="" />
             <button className="watch_video whitespace-nowrap">
               Watch Video
             </button>
           </span>
         </div>
-        <div className="hroSection_TapImg relative bottom-96 left-24">
-          <img src={heroSection_tapImg} alt="hero section Tap banner" />
-        </div>
+        {/* <div className="hroSection_TapImg"> */}
+        <img
+          className="hroSection_TapImg"
+          src={heroSection_tapImg}
+          alt="hero section Tap banner"
+        />
+        {/* </div> */}
       </div>
       <div className="hero_section_mobileOrder">
         <img
-          className="relative left-[80%] bottom-5"
+          className="hero_section_mobileOrder_start01"
           src={start01}
           alt="start 01"
         />
@@ -65,17 +69,17 @@ const HeroSection = () => {
         />
         <div className="heroSection_Mobile_imgsFlex">
           <img
-            className="heroSection_mobile_img_01 "
+            className="heroSection_mobile_img_01"
             src={heroSection_mobile_img_01}
             alt="mobile 01"
           />
           <img
-            className="heroSection_mobile_img_02 "
+            className="heroSection_mobile_img_02"
             src={heroSection_mobile_img_02}
             alt=" mobile 02"
           />
           <img
-            className="heroSection_mobile_img_03 "
+            className="heroSection_mobile_img_03"
             src={heroSection_mobile_img_03}
             alt="mobile 02"
           />
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,84 +2,90 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import uifry_logo from "../../images/uifry_logo.png";
 import pdfFile from "../../images/pdf-sample.pdf"; // Import the PDF file
+
 const Navbar = () => {
   interface navLinksType {
     name: string;
     href: string;
   }
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("Home"); // Track the active link
+
   const navLinks: navLinksType[] = [
     { name: "Home", href: "#" },
     { name: "About Us", href: "#" },
     { name: "Pricing", href: "#" },
     { name: "Features", href: "#" },
   ];
-  const [activeLink] = useState("Home");
 
   const mobileMenuVariants = {
     open: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.5, ease: "easeInOut" },
+      transition: { duration: 0.6, ease: "easeInOut" },
     },
     closed: {
       opacity: 0,
       y: -20,
-      transition: { duration: 0.5, ease: "easeInOut" },
+      transition: { duration: 0.6, ease: "easeInOut" },
     },
   };
+
   return (
-    <div>
-      <nav className="bg-white">
-        <div className="max-w-7xl mx-auto px-5 pt-10">
-          <div className="flex justify-between items-center mx-10">
-            {/* Logo */}
-            <div className="flex-shrink-0">
-              <img src={uifry_logo} alt="uifry logo" className="h-8 w-auto" />{" "}
-              {/* Added alt and size */}
-            </div>
+    <nav className="bg-white shadow-lg">
+      <div className="max-w-7xl mx-auto px-6 md:px-10 pt-6">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <div className="flex-shrink-0">
+            <img src={uifry_logo} alt="uifry logo" className="h-10 w-auto" />
+          </div>
 
-            {/* Desktop Menu */}
-            <div className="hidden md:flex space-x-8 md:justify-start md:mr-48">
-              {navLinks.map((link) => (
-                <a
-                  key={link.name}
-                  href={link.href}
-                  className={` text-black-700 hover:text-gray-600 px-3 py-2 rounded-md text-xl font-medium transition-colors ${
-                    activeLink === link.name
-                      ? "text-red-500 font-bold" // Active state styles
-                      : "text-black-700 hover:text-gray-600"
-                  }`}
-                >
-                  {link.name}
-                </a>
-              ))}
-            </div>
-            <a
-              className="items-center bg-black text-white px-7 py-3 hidden md:flex rounded-sm font-medium"
-              href={pdfFile} // Use the imported PDF file
-              download="sample-report.pdf" // Name of the downloaded file
-              aria-label="Download sample report"
-            >
-              Download
-            </a>
-
-            {/* Mobile Menu Button */}
-            <div className="md:hidden">
-              <button
-                type="button"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500"
-                aria-controls="mobile-menu"
-                aria-expanded={isMenuOpen}
+          {/* Desktop Menu */}
+          <div className="hidden md:flex md:justify-start md:absolute md:ml-48 space-x-4 ">
+            {navLinks.map((link) => (
+              <a
+                key={link.name}
+                href={link.href}
+                onClick={() => setActiveLink(link.name)}
+                className={` z-10 font-medium
+ px-3 py-2 text-xl  md:justify-start  transition-colors ${
+   activeLink === link.name
+     ? "text-red-500 font-semibold "
+     : "text-black  hover:text-red-500"
+ }`}
               >
-                <span className="sr-only">Open main menu</span>
-                {/* Hamburger Icon */}
+                {link.name}
+              </a>
+            ))}
+          </div>
+
+          {/* Download Button */}
+          <a
+            className="hidden md:flex bg-black text-white px-8 py-4 rounded-sm font-medium transition hover:bg-gray-800"
+            href={pdfFile}
+            download="sample-report.pdf"
+            aria-label="Download sample report"
+          >
+            Download
+          </a>
+
+          {/* Mobile Menu Button */}
+          <div className="md:hidden z-10">
+            <button
+              type="button"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="p-2 rounded-md text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:outline-none"
+              aria-controls="mobile-menu"
+              aria-expanded={isMenuOpen}
+            >
+              <span className="sr-only">Open main menu</span>
+              {/* Hamburger Icon */}
+              {!isMenuOpen ? (
                 <svg
-                  className={`${isMenuOpen ? "hidden" : "block"} h-6 w- 6`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
+                  className="h-6 w-6"
                   viewBox="0 0 24 24"
+                  fill="none"
                   stroke="currentColor"
                 >
                   <path
@@ -89,12 +95,11 @@ const Navbar = () => {
                     d="M4 6h16M4 12h16M4 18h16"
                   />
                 </svg>
-                {/* Close Icon */}
+              ) : (
                 <svg
-                  className={`${isMenuOpen ? "block" : "hidden"} h-6 w-6`}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
+                  className="h-6 w-6"
                   viewBox="0 0 24 24"
+                  fill="none"
                   stroke="currentColor"
                 >
                   <path
@@ -104,50 +109,54 @@ const Navbar = () => {
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
-              </button>
-            </div>
+              )}
+            </button>
           </div>
         </div>
+      </div>
 
-        {/* Mobile Menu */}
-        <AnimatePresence>
-          {isMenuOpen && ( // Conditionally render based on isMenuOpen
-            <motion.div
-              className="mobile_View_nav md:hidden flex flex-col w-full bg-white border-t border-gray-200"
-              variants={mobileMenuVariants}
-              initial="closed"
-              animate="open"
-              exit="closed"
-            >
-              <div className="px-2 pt-2 pb-3 space-y-1">
-                {navLinks.map((link) => (
-                  <a
-                    key={link.name}
-                    href={link.href}
-                    onClick={() => setIsMenuOpen(false)} // Close menu on link click
-                    className="text-gray-700 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium transition-colors text-center border-b-2 border-gray-500"
-                  >
-                    {link.name}
-                  </a>
-                ))}
-                {/* <span className="bg-black text-white px-5 py-4  rounded-md font-medium mt-6 w-52"> */}
-                <div
-                  className="bg-black text-white px-5 py-4  rounded-md font-medium mt-6 w-80 mx-auto"
-                  // href={pdfFile} // Use the imported PDF file
-                  // download="sample-report.pdf" // Name of the downloaded file
-                  aria-label="Download sample report"
+      {/* Mobile Menu */}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            className="z-50 absolute top-12  md:hidden flex flex-col w-full bg-white border-t border-gray-200 mt-4"
+            variants={mobileMenuVariants}
+            initial="closed"
+            animate="open"
+            exit="closed"
+          >
+            <div className="px-4 pt-4 pb-3 space-y-2">
+              {navLinks.map((link) => (
+                <a
+                  key={link.name}
+                  href={link.href}
+                  onClick={() => {
+                    setActiveLink(link.name);
+                    setIsMenuOpen(false);
+                  }}
+                  className={`block px-4 py-2 text-lg font-medium text-center border-b border-gray-400 transition-colors ${
+                    activeLink === link.name
+                      ? "text-red-500 font-semibold "
+                      : "text-gray-700 hover:text-red-500"
+                  }`}
                 >
-                  {" "}
-                  {/* Download button in mobile menu */}
-                  Download
-                </div>
-                {/* </span> */}
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </nav>
-    </div>
+                  {link.name}
+                </a>
+              ))}
+              {/* Mobile Download Button */}
+              <a
+                className="bg-black text-white px-5 py-3 rounded-md font-medium w-full block text-center mt-4"
+                href={pdfFile}
+                download="sample-report.pdf"
+                aria-label="Download sample report"
+              >
+                Download
+              </a>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </nav>
   );
 };
 
